d169edb40124ea2cff0d3b1e511464e6cffafdcb
TeeSSkooo <shemigonbogdanwork@gmail.com>
2023-03-14 16:09:34 +0200

    Added chat border to light theme

--- src/components/chat-room/ChatRoom.tsx
+++ src/components/chat-room/ChatRoom.tsx
@@ -4,18 +4,21 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 import MessageItem from 'components/message-item/MessageItem';
 import MessageForm from 'components/message-form/MessageForm';
 
+import useTheme from 'hooks/useTheme';
 import database from 'firebaseApp';
 
 import styles from './ChatRoom.module.css';
 
 const ChatRoom: React.FC = () => {
+  const { theme } = useTheme();
+
   const loadQuery = query(collection(database, 'messages'), orderBy('createdAt'));
   const [messages] = useCollectionData(loadQuery);
 
   return (
     <div className="py-[30px]">
       <div className="chat-container h-screen flex flex-col justify-between">
-        <div className={styles.chatField}>
+        <div className={styles.chatField} style={{ borderColor: `${theme === 'light' ? '#000' : '#fff'}` }}>
           {messages?.map((message) => (
             <MessageItem
               avatarUrl={message?.senderAvatar!}
